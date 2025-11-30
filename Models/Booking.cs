9cf43c793904db8e2416f96ea2d27dd4e1b6c761
jyneto <j-pm@live.se>
2025-11-30 22:25:53 +0100

    Enhance Booking class with table and guest details
    Added `TableId` and `TableNumber` properties to represent table details.
    Added `GuestId` and `GuestName` properties to represent guest details.
    These changes improve the Booking class by providing more explicit associations with tables and guests.

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -9,12 +9,17 @@ namespace MVCWebInvite.Models
 
         [Required, ForeignKey(nameof(Table))]
         public int FK_TableId { get; set; }
+        public int TableId { get; set; }
+        public int? TableNumber { get; set; }
+
+        public int GuestId { get; set; }
         public Table? Table { get; set; }
 
         [Required, ForeignKey(nameof(Guest))]
         public int FK_GuestId { get; set; }
         public Guest? Guest { get; set; }
 
+        public string? GuestName { get; set; }
         [Required]
         public DateTime StartTime { get; set; } //Utc
         [Required]
